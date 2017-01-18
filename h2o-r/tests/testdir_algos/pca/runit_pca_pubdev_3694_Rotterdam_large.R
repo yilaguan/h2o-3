b9131cec60514a2b544ca62651285f09bb6fe915
wendycwong <wendycwong@yahoo.com>
2017-01-18 09:10:57 -0800

    PUBDEV-3827: fix PCA scoring history and importance.  Added Runit test derived from Angela JIRA.
    PUBDEV-3797: Fix PCA scoring history and importance.  Fixed Python interface to add PCA method Randomized.
    
    PUBDEV-3827: fix PCA scoring history and importance.  Add compute_metrics to legal list of parameters for PCA in Python.
    
    PUBDEV-3827: fix PCA scoring history and importance.  Added pyunit test to check scoring history and importance for every PCA mode.
    
    PUBDEV-3827: Fix PCA scoring history and importance.  Fixed bug in java.  Parameters total_variance was not calculated.  Improved pyunit test to check out all transform mode and all PCA_methods.
    
    PUBDEV-3827. Fix PCA scoring history and importance.  Correct calculation of total_variance.
    
    PUBDEV-3827: fix pca scoring history and importance.  Fix total_variance calculation when PCA method is glrm.
    
    PUBDEV-3827: fix PCA scoring history and importance.  Incorporate Pasha code reviews.  Appreciate it!
    
    PUBDEV-3827: fix PCA scoring history and importance.  Added GramSVD as default setting for svd_method.

--- h2o-r/tests/testdir_algos/pca/runit_pca_pubdev_3694_Rotterdam_large.R
+++ h2o-r/tests/testdir_algos/pca/runit_pca_pubdev_3694_Rotterdam_large.R
@@ -22,7 +22,7 @@ check.pca.widedata.gramSVD <- function() {
     h2o_pca <- h2o.prcomp(df, k = ranks, x = x, transform = 'STANDARDIZE', seed=12345)      # train H2O PCA
 
     # the eigenvectors and eigvalues calculated from R and H2O are close but not equal
-    isFlipped1 <- checkPCAModel(h2o_pca, pcaR, tolerance=1)
+    isFlipped1 <- checkPCAModel(h2o_pca, pcaR, tolerance=1e-1, compare_all_importance=FALSE)
 
     Log.info("Compare Projections into PC space")
     predR <- predict(pcaR, dfR)
