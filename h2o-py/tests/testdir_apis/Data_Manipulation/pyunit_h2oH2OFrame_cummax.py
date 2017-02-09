1c280f8fda494358c40a8fb24e65d28c8d6a3662
wendycwong <wendycwong@yahoo.com>
2017-02-09 10:00:17 -0800

    PUBDEV-4003: add api tests for python data manipulation module.  Add pyunit test for cummax.

---
+++ h2o-py/tests/testdir_apis/Data_Manipulation/pyunit_h2oH2OFrame_cummax.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+import sys
+sys.path.insert(1,"../../../")
+from tests import pyunit_utils
+import h2o
+import numpy as np
+from h2o.utils.typechecks import assert_is_type
+from h2o.frame import H2OFrame
+
+
+def h2o_H2OFrame_cummax():
+    """
+    Python API test: h2o.frame.H2OFrame.cummax()
+
+    Copied from pyunit_cumsum_cumprod_cummin_cummax.py
+    """
+    h2o.connect()
+    python_obj=[list(range(10)), list(range(10))]
+    python_obj_transpose = np.transpose(python_obj)
+    foo = h2o.H2OFrame(python_object=python_obj)
+    foo_transpose =  h2o.H2OFrame(python_object=python_obj_transpose)
+    print("wow")
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(h2o_H2OFrame_cummax())
+else:
+    h2o_H2OFrame_cummax()
